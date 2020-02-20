2f4a61868005678ea679d9274d2e62d97e4f9f45
Hamalainen <thomas.hamalainen@live.se>
2020-02-20 13:40:42 +0100

    keep testing

--- vaderApp/index.js
+++ vaderApp/index.js
@@ -2,18 +2,23 @@
 
 const express = require("express");
 
- var app = express();
- var fs = require("fs");
+var app = express();
+var fs = require("fs");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://heroku_xxjk4spc:qb84gciu80d4euonvs2anfc9cp@ds163182.mlab.com:63182/heroku_xxjk4spc";
 
+const PORT = process.env.PORT || 3000;
 
 
+app.get('/', function (req, res) {
+  console.log("start");
+  console.log("wtf is gooioing on?");
+  res.end("start");
+});
 
- app.get('/create', function (req, res) {
-    // fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( "kalsjadjaskdkjdsalkjsdlkajdslksajdlksajdlkjsa" );
-      res.end("hshdhahdahdhadhahdahd");
+app.get('/create', function (req, res) {
+  console.log("kalsjadjaskdkjdsalkjsdlkajdslksajdlksajdlkjsa");
+  res.end("hshdhahdahdhadhahdahd");
 
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
@@ -27,58 +32,8 @@ var url = "mongodb://heroku_xxjk4spc:qb84gciu80d4euonvs2anfc9cp@ds163182.mlab.co
 });
 
 
-  //  });
-//  });
-
- var apiServer = app.listen(8081, function () {
-   var host = apiServer.address().address
-   var port = apiServer.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
- });
-
-
-
-
-// const express = require("express");
-// const path = require('path');
-// var router = express.Router();
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://heroku_xxjk4spc:qb84gciu80d4euonvs2anfc9cp@ds163182.mlab.com:63182/heroku_xxjk4spc";
-
-
-// const PORT = process.env.PORT || 3000;
-
-// const INDEX = path.join(__dirname, '/dist/vaderApp/index.html');
-
-// const app = express()
-//   .use(express.static(__dirname + '/dist/vaderApp'))
-//   .use((req, res) => res.sendFile(INDEX));
-//   // .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-// // var app = express();
-// // var fs = require("fs");
-
-
-
-
-
-// apiServer.get('/listUsers', function (req, res) {
-//   console.log('ajsdlkjasdklöjasdkljaskldjaskldjklöasdklasj')
-
-//   MongoClient.connect(url, function (err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("heroku_xxjk4spc");
-//     dbo.createCollection("vader", function (err, res) {
-//       if (err) throw err;
-//       console.log("Collection created!");
-//       db.close();
-//     });
-//   });
-// });
-
-//  var apiServer = app.listen(PORT, function () {
-//    var host = apiServer.address().address
-//    var port = apiServer.address().port
-//    console.log("Example app listening at http://%s:%s", host, port)
-//  });
+var apiServer = app.listen(PORT, function () {
+  var host = apiServer.address().address
+  var port = apiServer.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+});
